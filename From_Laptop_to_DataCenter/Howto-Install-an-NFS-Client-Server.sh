4d5661bc88c7634845751d5ffcede25b5e6dd11d
Daniele <daniele.cesini@gmail.com>
2023-05-03 19:16:54 +0200

    Update Howto-Install-an-NFS-Client-Server.sh

--- From_Laptop_to_DataCenter/Howto-Install-an-NFS-Client-Server.sh
+++ From_Laptop_to_DataCenter/Howto-Install-an-NFS-Client-Server.sh
@@ -19,8 +19,8 @@ exportfs -r
 #On the client:
  
  yum install nfs-utils
- mount -t nfs -o ro,nosuid <your_server_ip>:/data /data
+ # mount -t nfs -o ro,nosuid <your_server_ip>:/data /data   - you can skip this
  ll /data/
- umount /data
+ # umount /data  - you can skip this
  cat /etc/fstab
 <SERVER_PRIVATE_IP>:/data /data   nfs defaults        0 0
