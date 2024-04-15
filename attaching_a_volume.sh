19183253ae08f8bdf9a5b2114a2291911b632162
Daniele <daniele.cesini@gmail.com>
2024-04-15 17:33:37 +0200

    Update attaching_a_volume.sh

--- attaching_a_volume.sh
+++ attaching_a_volume.sh
@@ -1,16 +1,16 @@
 df -h             #check the mounted filesystem
 fdisk -l          #list the attached disks
-fdisk /dev/sdb    #use your disk label obtained by the previous command
+fdisk /dev/xvdf    #use your disk label obtained by the previous command
 
 ######## PAY ATTENTION AND AVOID FORMATTING THE SYSTEM DISK!!!!!!!!!!
-# now you have an interactive interface to create a new partition in the disk, use teh commands p, n, p, w keep the defaults
+# now you have an interactive interface to create a new partition in the disk, use the commands p, n, p, w (keep the defaults when prompted)
 # fdisk commands: p, n (primary partition and use the defaults), p, w
 
-mkfs.ext4 /dev/sdb1   # create the ext4 filesystem
+mkfs.ext4 /dev/xvdf1   # create the ext4 filesystem
 mkdir /data2          # create a mountpoint for the new filesystem
-yum install vim       # if not present
+yum install vim       # if not present   (or dnf install vim)
 vim /etc/fstab        # edit the fstab file
-# add this line at the end of the fstab file:    /dev/sdb1     /data2  ext4 defaults 0 0
+# add this line at the end of the fstab file:    /dev/xvdf1     /data2  ext4 defaults 0 0
 
 ###############################
 #Inspect the fstab file
@@ -24,10 +24,11 @@ vim /etc/fstab        # edit the fstab file
 # See man pages fstab(5), findfs(8), mount(8) and/or blkid(8) for more info
 #
 UUID=4a1c93d9-eb47-4f96-9f3d-920e52dc8cca /                       xfs     defaults        0 0
-/dev/sdb1     /data2  ext4 defaults 0 0
+/dev/xvdf1     /data2  ext4 defaults 0 0
 ##########################################################
 
 df -h
+systemctl daemon-reload
 mount -a    #mount all the filesystem listed in the fstab file
 df -h   #check the differences
 ll /data2
